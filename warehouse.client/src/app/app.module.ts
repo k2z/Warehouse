239092ed405628715902ce90bae88e6fc06c6d61
Daniil Blagov <daniil.unknown@yandex.ru>
2025-08-10 20:32:51 +0300

    Initial commit Works on Mac

---
+++ warehouse.client/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
+import { MeasuresComponent } from './pages/measures/measures.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+import { BalancesComponent } from './pages/balances/balances.component';
+import { IncomesComponent } from './pages/incomes/incomes.component';
+import { ShipmentsComponent } from './pages/shipments/shipments.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ResourcesComponent,
+    MeasuresComponent,
+    ClientsComponent,
+    BalancesComponent,
+    IncomesComponent,
+    ShipmentsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ warehouse.client/src/app/pages/measures/measures.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-measures',
+  templateUrl: './measures.component.html',
+  styleUrl: './measures.component.css'
+})
+export class MeasuresComponent {
+
+}
