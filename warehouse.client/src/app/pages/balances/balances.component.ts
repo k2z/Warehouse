722184dd7b6669a2c49814abecee6de83dec0c69
Daniil Blagov <daniil.unknown@yandex.ru>
2025-08-12 01:28:52 +0300

    Added basic PrimeNG table

--- warehouse.client/src/app/pages/balances/balances.component.ts
+++ warehouse.client/src/app/pages/balances/balances.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { MultiSelectModule } from 'primeng/multiselect';
 
 @Component({
     selector: 'app-balances',
     templateUrl: './balances.component.html',
     styleUrl: './balances.component.css',
+    imports: [
+        FormsModule,
+        TableModule,
+        MultiSelectModule,
+    ],
 })
 export class BalancesComponent {
-
+    console = console;
+    public loading: boolean = false;
+    public balanceValues: any[] = [
+        { resource: 'Rice', measure: 'kg', quantity: 10 },
+        { resource: 'Sesame', measure: 'kg', quantity: 10 },
+        { resource: 'Hemp', measure: 'kg', quantity: 10 },
+        { resource: 'Rice', measure: 'ISO container', quantity: 10 },
+        { resource: 'Sesame', measure: 'ISO container', quantity: 10 },
+        { resource: 'Hemp', measure: 'ISO container', quantity: 10 },
+    ];
+    public resourceFilterOptions: any[] = [{ name: 'Rice', value: 'Rice' }, { name: 'Sesame', value: 'Sesame' }, { name: 'Hemp', value: 'Hemp' }];
 }
